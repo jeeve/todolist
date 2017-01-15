36b465a3f1fb8da97d9a284d81ff6af743c4ab8c
jeeve <jvjulien@free.fr>
2017-01-15 17:01:29 +0100

    bug server

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ on en crée une vide sous forme d'array avant la suite */
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
