7495cc473ba50915f2bd961dab1d6a25b6ba040c
jeeve <jvjulien@free.fr>
2017-01-15 16:58:00 +0100

    listen port

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ on en crée une vide sous forme d'array avant la suite */
 /* On redirige vers la todolist si la page demandée n'est pas trouvée */
 .use(function(req, res, next){
     res.redirect('/todo');
-})
+});
 
-.listen(process.env.PORT || 8080, '127.0.0.1');   
+//.listen(process.env.PORT || 8080, '127.0.0.1');   
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
